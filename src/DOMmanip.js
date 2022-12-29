7df6ecd8c06083b627c5a693d9d02136b952eb5a
Jonathan Hawes <jhawes580@gmail.com>
2022-12-29 10:09:20 -0800

    Implement working delete and update Projects still not finished.

--- src/DOMmanip.js
+++ src/DOMmanip.js
@@ -2,16 +2,19 @@ import ThickArrow from './icons/arrow-thicker.svg'
 import Edit from './icons/edit_square_FILL0_wght400_GRAD0_opsz48.svg'
 import Danger from './icons/dangerous_FILL0_wght400_GRAD0_opsz48.svg'
 import { format, formatDistance, formatRelative, parseISO, addDays } from 'date-fns';
-
+import { controller } from './index.js'
 
 
 // Adds the necessary event listeners on page load
 export const loadEventListeners = () => {
     document.querySelector('.create')
     .addEventListener('click', newToDoForm().open);
+    document.querySelector('.create')
+    .addEventListener('click', newToDoForm().newify);
 }
 
 export const newToDoForm = () => {
+
     const open = () => {
         document.querySelector('.to-do-overlay')
         .classList.remove('hidden');
@@ -29,10 +32,89 @@ export const newToDoForm = () => {
                 .removeEventListener('click', close)
     }
 
-    return { open, close }; 
+    const newify = () => {
+        // Replace with a clone to remove all event listeners
+        let buttonClone = document.querySelector('.submit');
+        document.querySelector('.submit').remove();
+        document.querySelector('.fieldset-wrapper').appendChild(buttonClone.cloneNode(true))
+        // Add event listener for making new todo on submit
+        document.querySelector('.submit')
+        .addEventListener('click', controller().createNewToDo);
+
+        document.querySelector('.fieldset-wrapper > legend').textContent = 'Add New Task';
+
+
+        // Make the form an easy to understand variable
+        let form = document.querySelector('#addNewToDo');
+
+        // Wipe the form values
+        const cleanForm = (userValue) => {
+            form.elements[`${userValue}`].value = null;
+        }
+        cleanForm('task');
+        cleanForm('date');
+        cleanForm('priority');
+        cleanForm('notes');
+        cleanForm('project');
+
+        document.querySelector('.fieldset-wrapper > .submit').textContent = 'Submit';
+    }
+
+    const updateify = (newArr, category, event) => {
+        // Replace with a clone to remove all event listeners
+        let buttonClone = document.querySelector('.submit');
+        document.querySelector('.submit').remove();
+        document.querySelector('.fieldset-wrapper').appendChild(buttonClone.cloneNode(true))
+    
+        
+        // Go up the event path to find uniqueID
+        let removeID = event.composedPath()[2].getAttribute('data-id');
+        let toDo = 0;
+        
+        console.log(newArr)
+        console.log(removeID)
+        for (let i = 0; i < newArr.length; i++) {
+            if (newArr[i].uniqueID == removeID) {
+                // This is the todo we are going to update
+                toDo = newArr[i];
+                newToDoForm().open();
+                // Add event listener for updating todo on submit, send in location and current category
+                document.querySelector('.submit')
+                .addEventListener('click', controller().updateToDo.bind(null, i, category, toDo.uniqueID));
+                break;
+            }
+        }
+        
+        document.querySelector('.fieldset-wrapper > legend').textContent = 'Update Task';
+    
+        // Make the form an easy to understand variable
+        let form = document.querySelector('#addNewToDo');
+    
+        console.log(toDo)
+        const changeForm = (userValue) => {
+            form.elements[`${userValue}`].value = toDo[`${userValue}`];
+        }
+
+        changeForm('task');
+        changeForm('date');
+        changeForm('priority');
+        changeForm('notes');
+        changeForm('project');
+
+        document.querySelector('.fieldset-wrapper > .submit').textContent = 'Update';
+    }
+
+    return { open, close, newify, updateify }; 
 }
 
 export const category = () => {
+
+    const _addToDoListeners = (toDoArr, category) => {
+        document.querySelectorAll(`.delete`)
+        .forEach(element => element.addEventListener('click', controller().deleteToDo.bind(null, toDoArr, category)))
+        document.querySelectorAll(`.edit`)
+        .forEach(element => element.addEventListener('click', newToDoForm().updateify.bind(null, toDoArr, category)))
+    }
 
     const _unload = () => {
         // First, hide all the headers
@@ -44,17 +126,19 @@ export const category = () => {
     }
 
     const load = (toDoArr, category) => {
-        
         // Unload all todos
         _unload();
-
         /* Create an array with only the todos that will be loaded 
         in the currently selected array */
         const newArr = toDoArr.filter(todo => (todo.categories.includes(category)))
         console.log(newArr);
         console.log(toDoArr);
 
+        // Build each todo in the new array
         newArr.forEach(todo => buildToDo(todo, category));
+        
+        // Add the listeners to the new todos
+        _addToDoListeners(toDoArr, category);
     }
 
     const highlight = (category) => {
@@ -75,22 +159,23 @@ const buildToDo = (newToDo, category) => {
     let li = document.createElement('li');
     let img = document.createElement('img');
     let span = document.createElement('span');
-
     
     content.querySelector(`.${category}-group`).classList.remove('hidden');
     content.querySelector(`.${category}-group`).appendChild(li.cloneNode(true)).classList.add('to-do-item');
+    content.querySelector(`.${category}-group > .to-do-item:last-child`).setAttribute('data-id', newToDo.uniqueID);
     content.querySelector(`.${category}-group > .to-do-item:last-child`).appendChild(div.cloneNode(true));
     content.querySelector(`.${category}-group > .to-do-item:last-child > div`).appendChild(div.cloneNode(true)).classList.add('check-circle');
     content.querySelector(`.${category}-group > .to-do-item:last-child > div > div`).classList.add('hidden');
     content.querySelector(`.${category}-group > .to-do-item:last-child`).appendChild(p.cloneNode(true)).textContent = newToDo.task;
     content.querySelector(`.${category}-group > .to-do-item:last-child`).appendChild(span.cloneNode(true)).classList.add('options');
     content.querySelector(`.${category}-group > .to-do-item:last-child > .options`).appendChild(p.cloneNode(true)).classList.add('date');
-    content.querySelector(`.${category}-group > .to-do-item:last-child > .options .date`).textContent = newToDo.dueDate;
+    content.querySelector(`.${category}-group > .to-do-item:last-child > .options .date`).textContent = newToDo.date;
     content.querySelector(`.${category}-group > .to-do-item:last-child > .options`).appendChild(img.cloneNode(true)).setAttribute('src', Edit);
     content.querySelector(`.${category}-group > .to-do-item:last-child > .options`).lastChild.setAttribute('alt', 'edit');
     content.querySelector(`.${category}-group > .to-do-item:last-child > .options`).lastChild.classList.add('edit');
     content.querySelector(`.${category}-group > .to-do-item:last-child > .options`).appendChild(img.cloneNode(true)).setAttribute('src', Danger);
     content.querySelector(`.${category}-group > .to-do-item:last-child > .options`).lastChild.setAttribute('alt', 'delete');
     content.querySelector(`.${category}-group > .to-do-item:last-child > .options`).lastChild.classList.add('delete');
+    
 }
 
--- src/index.js
+++ src/index.js
@@ -3,65 +3,125 @@ import { loadPage } from './buildPage.js';
 import * as DOMmanip from './DOMmanip.js';
 import { format, formatDistance, formatRelative, parseISO, addDays } from 'date-fns';
 
-const toDoItem = (task, dueDate, priority, notes, project) => {
+const toDoItem = (task, date, priority, notes, project, uniqueID) => {
     let completed = false;
     let categories = [];
-    return { task, dueDate, priority, notes, project, completed, categories };
+    return { task, date, priority, notes, project, completed, categories, uniqueID };
 }
 
-function createNewToDo(toDoArr, event) {
+// Define uniqueID as 1
+let uniqueID = 1;
 
-    // Stops page from refreshing
-    event.preventDefault();
-
-    /* Set category equal to whatever category header is 
-    bold since that's what's selected */
-    let category = categories().findCategory
-    (document.querySelector('.category > .bold').textContent) 
+// define the todo array
+let toDoArr = [];
 
 
-    // Make the form an easy to understand variable
-    let form = document.querySelector('#addNewToDo');
+export const controller = () => {
 
-    // Construct the new To-do
-    let newToDo = toDoItem(form.elements['task'].value,
-    form.elements['date'].value, form.elements['priority'].value,
-    form.elements['notes'].value, form.elements['project'].value)
+    const createNewToDo = (event) => {
 
-    // Add this new To-do to the array that keeps track of all todos
-    toDoArray(toDoArr).addToDo(newToDo);
+        // Stops page from refreshing
+        event.preventDefault();
     
-    // Now close the overlay
-    DOMmanip.newToDoForm().close();
+        /* Set category equal to whatever category header is 
+        bold since that's what's selected */
+        let category = categories().findCategory
+        (document.querySelector('.category > .bold').textContent) 
+    
+    
+        // Make the form an easy to understand variable
+        let form = document.querySelector('#addNewToDo');
+    
+        // Construct the new To-do
+        let newToDo = toDoItem(form.elements['task'].value,
+        form.elements['date'].value, form.elements['priority'].value,
+        form.elements['notes'].value, form.elements['project'].value,
+        uniqueID)
+        
+        // Iterate unique ID so the next todo doesnt get the same number
+        uniqueID++;
+    
+        // Add this new To-do to the array that keeps track of all todos
+        toDoArray().addToDo(toDoArr, newToDo);
+        
+        // Now close the overlay
+        DOMmanip.newToDoForm().close();
+    
+        // Set the categories for the todo
+        categories().setCategories(newToDo);
+    
+        // Finally, reload the selected category
+        DOMmanip.category().load(toDoArr, category);
+    }
 
-    // Set the categories for the todo
-    categories().setCategories(newToDo);
+    const updateToDo = (location, category, sameID, event) => {
+        // Stops page from refreshing
+        event.preventDefault();
 
-    // Finally, reload the selected category
-    DOMmanip.category().load(toDoArr, category);
+        // Now close the overlay
+        DOMmanip.newToDoForm().close();
+
+        // Make the form an easy to understand variable
+        let form = document.querySelector('#addNewToDo');
+        
+        // Make a new todo with the updated values
+        let newToDo = toDoItem(form.elements['task'].value,
+        form.elements['date'].value, form.elements['priority'].value,
+        form.elements['notes'].value, form.elements['project'].value,
+        sameID)
+        
+        // Set the categories for the todo
+        categories().setCategories(newToDo);
+        
+        // Set the old todo equal to the new one.
+        toDoArr[location] = newToDo;
+        
+        // Load the current category
+        DOMmanip.category().load(toDoArr, category)
+    }
+
+    const deleteToDo = (newArr, category, event) => {
+        
+        // Go up the event path to find uniqueID
+        let removeID = event.composedPath()[2].getAttribute('data-id');
+        console.log(newArr)
+        for (let i = 0; i < newArr.length; i++) {
+            if (newArr[i].uniqueID == removeID) {
+                newArr = toDoArray().unaddToDo(newArr, i);
+                break;
+            }
+        }
+    
+        toDoArr = newArr;
+    
+        DOMmanip.category().load(toDoArr, category);   
+    }
+
+    return { createNewToDo, updateToDo, deleteToDo }
 }
 
-const toDoArray = (toDoArr) => {
+const toDoArray = () => {
 
-    const addToDo = (todo) => {
+    const addToDo = (toDoArr, todo) => {
         toDoArr.push(todo);
-        console.log(toDoArr)
     }
 
-    const removeToDo = (location) => {
-        // Cut out the desired todo spot
-        toDoArr = toDoArr.slice(0, location-1) + toDoArr.slice(location+1);
+    const unaddToDo = (newArr, location) => {
+        // Cut out the selected todo
+        newArr = toDoArr.slice(0, location);
+        newArr.push(...toDoArr.slice(location+1));
+        return newArr;
     }
 
-    const viewToDo = (todo) => {
+    const viewToDo = () => {
         return toDoArr;
     }
 
-    const updateToDo = (location, newToDo) => {
+    const overwriteToDo = (location, newToDo) => {
         toDoArr[location] = newToDo;
     }
 
-    return { addToDo, viewToDo, removeToDo, updateToDo };
+    return { addToDo, viewToDo, unaddToDo, overwriteToDo };
 }
 
 const categories = () => {
@@ -78,24 +138,27 @@ const categories = () => {
     }
 
     const setCategories = (newToDo) => {
+        // Wipe the categories so we don't repeat them
+        newToDo.categories = [];
         // Make the current date comparable with the due date format
         let currentDate = format(new Date(), 'yyyy-MM-dd');
         // Every todo will be in the all category
         newToDo.categories.push('inbox');
 
         // Decide which categories there will be for this todo
-        if (newToDo.dueDate < currentDate) {
+        if (newToDo.date < currentDate) {
             newToDo.categories.push('pastdue');
-        } if (newToDo.dueDate > currentDate && newToDo.dueDate <= addDays(parseISO(currentDate), 7)) {
+        } if (newToDo.date > currentDate && newToDo.date <= addDays(parseISO(currentDate), 7)) {
             newToDo.categories.push('thisweek');
-        } if (newToDo.dueDate == currentDate) {
+        } if (newToDo.date == currentDate) {
             newToDo.categories.push('today');
         } if (newToDo.priority == 'high') {
             newToDo.categories.push('highpriority');
         }
     }
 
-    const updateCategory = (toDoArr, event) => {
+    const updateCategory = (event) => {
+        console.log(toDoArr)
 
         // Set category equal to the text content of the clicked item
         let category = findCategory(event.target.textContent);
@@ -113,13 +176,7 @@ const categories = () => {
     loadPage();
     DOMmanip.loadEventListeners();
 
-    // define the todo array
-    const toDoArr = [];
-
     // add an event listener to all the cateGory elements to update the variable
     document.querySelectorAll('.category')
-    .forEach(element => element.addEventListener('click', categories().updateCategory.bind(null, toDoArr)))
-    // when a form is submitted, create and add the todo.
-    document.querySelector('.submit')
-    .addEventListener('click', createNewToDo.bind(null, toDoArr));
+    .forEach(element => element.addEventListener('click', categories().updateCategory))
 })()
